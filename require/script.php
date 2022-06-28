dc6e31609d65e70f2e884640881867c3f2bb8e12
Snow Bunny <marylynemutindi@gmail.com>
2022-06-28 10:01:52 -0400

    first commit

---
+++ require/script.php
@@ -0,0 +1,56 @@
+<!-- main jQuery -->
+<script src="../js/jquery.js"></script>
+<!-- bootstrap -->
+<script src="../js/bootstrap.min.js"></script>
+<!-- bx slider -->
+<script src="../js/jquery.bxslider.min.js"></script>
+<!-- count to -->
+<script src="../js/jquery.countTo.js"></script>
+<!-- owl carousel -->
+<script src="../js/owl.carousel.min.js"></script>
+<!-- validate -->
+<script src="../js/validate.js"></script>
+<!-- mixit up -->
+<script src="../js/jquery.mixitup.min.js"></script>
+<!-- easing -->
+<script src="../js/jquery.easing.min.js"></script>
+<!-- gmap helper -->
+<script src="../../../maps.googleapis.com/maps/api/js2a71?key=AIzaSyAHzPSV2jshbjI8fqnC_C4L08ffnj5EN3A"></script>
+<!--gmap script-->
+<script src="../js/gmaps.js"></script>
+<script src="../js/map-helper.js"></script>
+<!-- video responsive script -->
+<script src="../js/jquery.fitvids.js"></script>
+<!-- jQuery ui js -->
+<script src="../assets/jquery-ui-1.11.4/jquery-ui.js"></script>
+<!-- Language Switche  -->
+<script src="../assets/language-switcher/jquery.polyglot.language.switcher.js"></script>
+<!-- fancy box -->
+<script src="js/jquery.fancybox.pack.js"></script>
+<script src="js/jquery.appear.js"></script>
+<!-- isotope script-->
+<script src="../js/isotope.js"></script>
+<script src="../js/jquery.prettyPhoto.js"></script> 
+<!-- jQuery timepicker js -->
+<script src="../assets/timepicker/timePicker.js"></script>
+<!-- Bootstrap select picker js -->
+<script src="../ssets/bootstrap-sl-1.12.1/bootstrap-select.js"></script>                               
+
+
+<!-- revolution slider js -->
+<script src="../assets/revolution/js/jquery.themepunch.tools.min.js"></script>
+<script src="../assets/revolution/js/jquery.themepunch.revolution.min.js"></script>
+<script src="../assets/revolution/js/extensions/revolution.extension.actions.min.js"></script>
+<script src="../assets/revolution/js/extensions/revolution.extension.carousel.min.js"></script>
+<script src="../assets/revolution/js/extensions/revolution.extension.kenburn.min.js"></script>
+<script src="../assets/revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
+<script src="../assets/revolution/js/extensions/revolution.extension.migration.min.js"></script>
+<script src="../assets/revolution/js/extensions/revolution.extension.navigation.min.js"></script>
+<script src="../assets/revolution/js/extensions/revolution.extension.parallax.min.js"></script>
+<script src="../assets/revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+<script src="../assets/revolution/js/extensions/revolution.extension.video.min.js"></script>
+
+
+
+<!-- thm custom script -->
+<script src="../js/custom.js"></script>
